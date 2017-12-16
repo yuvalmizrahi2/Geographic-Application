3634e9538047412dda45b2e86f9147c4b465b1a8
avivro <aviv6.ro@gmail.com>
2017-12-17 00:01:29 +0200

    added junit

---
+++ src/testproject/IOCsvFileTest.java
@@ -0,0 +1,32 @@
+package testproject;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import classproject.CsvToSamples;
+import classproject.IOCsvFile;
+import classproject.Sample;
+
+class IOCsvFileTest {
+
+	@Test
+	void testReadfile() {
+		ArrayList<Sample> test = IOCsvFile.readfile("filetest");
+		assertTrue(test.size() == 3821);
+
+	}
+
+	@Test
+	void testWritefile() {
+		ArrayList<Sample> test = IOCsvFile.readfile("filetest");
+		IOCsvFile.writefile("IOCsvFileTest.csv", test);
+		test = CsvToSamples.readfile("IOCsvFileTest.csv");
+		assertTrue(test.size() == 3607);
+	}
+
+}
